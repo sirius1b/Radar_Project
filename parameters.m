2772f9ef94c7df5a256377725dcd48987b286a69
broompa <lavanya18155@iiitd.ac.in>
2020-12-17 18:41:37 +0530

    Files

---
+++ parameters.m
@@ -0,0 +1,86 @@
+%%
+clc 
+clear 
+close all
+
+%% Consants
+K2a = 0.01; %Nm/A
+K3a = 0.01; %V/rad/s
+Ra  = 1; %ohm 
+La  = 0.5;%H
+Ja  = 500; %kgm^2
+ba = 0.1; %Nms
+
+K2e = 0.01; %Nm/A
+K3e = 0.01; %V/rad/s
+Re  = 1; %ohm 
+Le  = 0.5;%H
+Je = 600; %kgm^2
+be = 0.1;%Nms
+
+%educated guess:
+M_e = 60; %kg
+Len_e = sqrt(300); %m
+
+g = 9.81; %m/s^2 
+%% State vector 
+%% Matrices (state space)
+
+A = [0, 1, 0, 0, 0, 0;
+     0, 0, 1, 0, 0, 0;
+     0, -((Ra*ba)/(La*Ja) + (K3a *K2a)/(La*Ja)), -((Ra/La) + (ba/Ja)), 0 , 0 , 0 ;
+     0, 0, 0, 0, 1, 0;
+     0, 0, 0, 0, 0, 1;
+     0, 0, 0, 0, -((Re*be)/(Le*Je) + (K3e *K2e)/(Le*Je)), -((Re/Le) + (be/Je));
+    ];
+B = [0, 0;
+     0, 0;
+     K2a/(La*Ja), 0;
+     0, 0;
+     0, 0;
+     0, K2e/(Le*Je);];
+
+%% output C, D
+C = [1, 0, 0, 0, 0, 0;
+    0, 0, 0, 1, 0, 0;];
+D = 0;
+Cr = C;
+%% Build system
+sys = ss(A,B,C,D);
+ctr = ctrb(sys);
+obs = obsv(sys);
+% min(size(sys)) == rank(ctr); %full rank
+% min(size(sys)) == rank(obs); %full rank
+x0 = [0, 0, 0, 0, 0, 0]';
+
+%% Controller
+des_poles_ctrb = [-9, -9, -10, -10, -11, -11]';
+des_poles_obsv = [-12,-12,-13,-13,-14,-14];
+K = place(A, B, des_poles_ctrb);
+L = place(A',C',des_poles_obsv)';
+
+%% some reference state 
+x_r = [pi/6 , 0, 0, pi/4, 0, 0]';
+
+%% for integrator
+n = 6;
+m = 2;
+
+A1 = [A , zeros(n,m) ; 
+      -Cr ,zeros(m,m);];
+B1 = [B;zeros(m,m)];
+% rank(ctrb(A1,B1))
+des_poles_Int = [des_poles_ctrb; -12; -12];
+K_Int = place(A1,B1,des_poles_Int);
+K = K_Int(:,1:n);
+Ki = K_Int(:,n+1:end);
+
+xr = zeros(n,1);
+ref_state = [pi/6;pi/4];
+
+%% Object tracking
+radar_position = [0;0;0] ; % x,y,z
+v = 50 ; %m/s
+z = 1e4; %m
+
+
